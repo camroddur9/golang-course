31569f4723e40be9a2e79a9fe2de7e4bd8e400a8
camilorod99 <crodriguez@umbra-team.com>
2021-05-16 21:42:23 -0500

    second class

--- src/main.go
+++ src/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//Constant declaration
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println(pi, pi2)
+
+	//Variables declaration
+	base := 12
+	var altura int = 14
+	fmt.Println(base * altura)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
 }
